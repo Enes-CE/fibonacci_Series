5a58f9566a1ca18fadc9c7d74050b0b231e501c0
İbrahim Enes <71263103+Enes-CE@users.noreply.github.com>
2021-03-13 18:47:11 +0300

    Update fibonacciSeries.java

--- fibonacciSeries.java
+++ fibonacciSeries.java
@@ -3,12 +3,12 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Lütfen terim sayısını giriniz : ");
-    int number = scanner.nextInt();
-     int sayi1 = 1;
-     int sayi2 = 1;
-     int sayi3 ;
+    int term = scanner.nextInt();
+    int sayi1 = 1;
+    int sayi2 = 1;
+    int sayi3 ;
 
-    for(int i = 0; i<number ; ++i){
+    for(int i = 0; i<term ; ++i){
 
         sayi3=sayi1+sayi2;
         sayi1=sayi2;
